c5a45588e150d4cf6f2ae4df4e193a92854a97e2
zhaojiedi <zhaojiedi@bytedance.com>
2022-08-30 09:25:42 +0800

    v1

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ class ForeverMemoryClient:
         self.screenshot_cnt = 0
         self.seal_cnt = 0
         self.hour = -1
-        self.nongzuowei="nangua"
-        #self.nongzuowei = "xrz"
+        self.nongzuowei = "nangua"
+        # self.nongzuowei = "xrz"
 
         if "darwin" in sys.platform:
             self.os = "mac"
@@ -44,14 +44,14 @@ class ForeverMemoryClient:
         pic_file = "imgs/" + self.os + "/" + pic_file
         self.log("start find  desc={},pic_file={},cnt={},confidence={}".format(
             desc, pic_file, cnt, confidence))
-        for i in range(1, cnt):
-            if i == 1:
+        for i in range(0, cnt):
+            if i == 0:
                 self.log(
                     "start find {},desc={},pic_file={},cnt={},confidence={}".format(i, desc, pic_file, cnt,
                                                                                     confidence))
             else:
                 self.log("retry find desc={},pic_file={},cnt={},current_cnt={},confidence={}".format(
-                    desc,pic_file, cnt, i, confidence))
+                    desc, pic_file, cnt, i, confidence))
             if region is None:
                 pos = pyautogui.locateCenterOnScreen(pic_file, confidence=confidence, region=region)
             else:
@@ -61,7 +61,7 @@ class ForeverMemoryClient:
                     desc, pic_file, cnt, i, confidence))
                 return pos
 
-            time.sleep(0.92)
+            time.sleep(0.43)
         self.log("not get desc={},pic_file={},cnt={},current_cnt={},confidence={}".format(
             desc, pic_file, cnt, cnt, confidence))
         return None
@@ -77,74 +77,36 @@ class ForeverMemoryClient:
             pyautogui.doubleClick()
         time.sleep(1)
 
-    def run(self):
-        # record start info
-        start_message = "start,with first_start_delay={}".format(self.first_start_delay)
-        print(start_message)
-        self.log(start_message)
-        # time.sleep(self.first_start_delay)
-
-        self.print_size()
-        # self.test_move()
-        # self.prepare_works()
-        while True:
-            try:
-                hour = time.localtime().tm_hour
-                if hour != self.hour:
-                    self.hour = hour
-                    self.prepare_works()
-
-                self.run_one_v2()
-            except Exception as err:
-                self.log("error={}".format(err))
-
-
-    def run_one_v2(self):
-        self.goto_home()
-        self.open_xrz()
-        if self.can_seal():
-            self.seal()
-        self.goto_xrz()
-        time.sleep(self.seal_interval)
-
     def run_sealonly(self):
         while True:
             self.seal()
             time.sleep(self.seal_interval)
+
     def run_v3(self):
-        # record start info
+
         start_message = "start,with first_start_delay={}".format(self.first_start_delay)
         print(start_message)
         self.log(start_message)
-        # time.sleep(self.first_start_delay)
-
-        self.print_size()
-        # self.test_move()
         self.prepare_works()
         while True:
             try:
-                # hour = time.localtime().tm_hour
-                # if hour != self.hour:
-                pos = self.while_get_pic("nongzuowu.png", 5, 0.6, "nongzuowu",region=None)
-                #break
+                pos = self.while_get_pic("nongzuowu.png", 5, 0.6, "nongzuowu", region=None)
                 if pos is None:
-                    # self.hour = hour
+                    time.sleep(60)
                     self.prepare_works()
-
                 self.seal()
                 time.sleep(self.seal_interval)
             except Exception as err:
                 self.log("error={}".format(err))
 
-
     def seal(self):
-            self.seal_cnt += 1
-            self.log("start seal {}".format(self.seal_cnt))
-            pyautogui.keyDown('shift')
-            pyautogui.click()
-            pyautogui.keyUp('shift')
-            self.log("end seal {}".format(self.seal_cnt))
-            #pyautogui.press('esc')
+        self.seal_cnt += 1
+        self.log("start seal {}".format(self.seal_cnt))
+        pyautogui.keyDown('shift')
+        pyautogui.click()
+        pyautogui.keyUp('shift')
+        self.log("end seal {}".format(self.seal_cnt))
+        # pyautogui.press('esc')
 
     def can_seal(self):
         return True
@@ -162,13 +124,13 @@ class ForeverMemoryClient:
         # self.entry_server()
         self.close()
         self.open()
-        time.sleep(20)
+        # time.sleep(20)
         # self.login_hub()
-        # time.sleep(2)
+        time.sleep(2)
         self.login_with_code()
-        time.sleep(1)
+        # time.sleep(1)
         self.choice_shengcun()
-        time.sleep(1)
+        # time.sleep(1)
         self.login_into_shengcun()
         time.sleep(30)
         if self.nongzuowei == "nangua":
@@ -182,7 +144,6 @@ class ForeverMemoryClient:
             time.sleep(10)
             self.open_xrz()
             time.sleep(1)
-
 
     def print_size(self):
         screenWidth, screenHeight = pyautogui.size()
@@ -201,12 +162,16 @@ class ForeverMemoryClient:
         # pyautogui.screenshot("pics/{}_{}_{}.png".format(self.run_cnt, self.pic_cnt, filename))
         self.screenshot_cnt += 1
 
+    def register(self):
+        pyautogui.write('/register  ' + self.code + " " + self.code, self.default_input_interval)
+        pyautogui.press('enter')
+
     def login_with_code(self):
         pyautogui.write('/login  ' + self.code, interval=self.default_input_interval)
         self.screenshot("login_with_code01")
         pyautogui.press('enter')
         self.screenshot("login_with_code02")
-        time.sleep(0.5)
+        time.sleep(0.2)
 
     def choice_shengcun(self):
         self.log("start choice shengcun")
@@ -214,26 +179,18 @@ class ForeverMemoryClient:
         pyautogui.write("/bs bc", interval=self.default_input_interval)
         pyautogui.press('enter')
 
-    # choice shengcun server , and goto
     def login_into_shengcun(self):
-        #pyautogui.click(button='right')
-        # ime.sleep(0.3)
         pos = self.while_get_pic('shengcun.png', cnt=5, confidence=0.85, desc="shengcun-server", region=None)
-
         self.screenshot("login_into_shengcun")
         self.click_pos(pos, "shengcun-server", 2)
-        # time.sleep(10)
-        # pyautogui.press('\t', 'enter')
-        time.sleep(1)
-        #pyautogui.hotkey("")
+        time.sleep(0.2)
 
-    # # goto anniu kaiguan
-    # def goto_home(self):
-    #     pyautogui.write('/home home', interval=self.default_input_interval)
-    #     self.screenshot("goto_home")
-    #     pyautogui.press('enter')
-    #     self.screenshot("goto_home")
-    #     time.sleep(5)
+    def goto_area_2(self):
+        pyautogui.write('/bs', interval=self.default_input_interval)
+        pyautogui.press('enter')
+        pos = self.while_get_pic('erqu.png', cnt=2, confidence=0.82, desc="xrz-seal-button",region=None)
+        self.click_pos(pos, "first-seal-xrz", 1)
+        time.sleep(0.2)
 
     def goto_xrz(self):
 
@@ -243,7 +200,7 @@ class ForeverMemoryClient:
         self.screenshot("goto_xrz02")
 
     def goto_nangua(self):
-        pyautogui.write('/home h3', interval=self.default_input_interval)
+        pyautogui.write('/res tp panda_nangua.gj', interval=self.default_input_interval)
         self.screenshot("goto_nangua")
         pyautogui.press('enter')
         self.screenshot("goto_nangua")
@@ -254,7 +211,8 @@ class ForeverMemoryClient:
         pyautogui.press('enter')
         pyautogui.moveTo(100, 100)
         time.sleep(0.3)
-        pos = self.while_get_pic('xrz.png', cnt=2, confidence=0.82, desc="xrz-seal-button", region=(2000/2, 800/2, 2500/2, 1100/2))
+        pos = self.while_get_pic('xrz.png', cnt=2, confidence=0.82, desc="xrz-seal-button",
+                                 region=(2000 / 2, 800 / 2, 2500 / 2, 1100 / 2))
         self.screenshot("open_xrz01")
         self.click_pos(pos, "first-seal-xrz", 1)
         self.screenshot("open_xrz02")
@@ -274,66 +232,88 @@ class ForeverMemoryClient:
         self.log("pos={}".format(pos))
         time.sleep(0.3)
 
-    def run2(self):
-        # record start info
-        start_message = "start,with first_start_delay={}".format(self.first_start_delay)
-        print(start_message)
-        self.log(start_message)
-        time.sleep(self.first_start_delay)
-
-        self.print_size()
-        self.test_move()
-        #self.prepare_works()
-        while True:
-            try:
-                self.seal()
-                time.sleep(self.seal_interval)
-            except Exception as err:
-                self.log("error={}".format(err))
 
     def close(self):
-        cmd1=""" ps aux |grep MultiMC.app |grep -v grep  |grep MultiMC.app |awk '{print $2}' |while read pid ; do kill -9 $pid  ; done """
+        cmd1 = """ ps aux |grep MultiMC.app |grep -v grep  |grep MultiMC.app |awk '{print $2}' |while read pid ; do kill -9 $pid  ; done """
         os.system(cmd1)
-        time.sleep(2)
-
+        time.sleep(0.5)
         pass
 
-    def open(self):
+    def sleep_while(self, sleep_interval, max_sleep_seconds, pic_file, desc, confidence):
+        current_sleep_seconds = 0
+        while True:
+            pos = self.while_get_pic(pic_file, cnt=1, confidence=confidence, desc=desc, region=None)
+            if pos is not None:
+                return True
+            if current_sleep_seconds > max_sleep_seconds:
+                return False
+            time.sleep(sleep_interval)
+
+    def open(self,user):
         os.system("open /Applications/MultiMC.app")
-        time.sleep(5)
-        pos = self.while_get_pic('instance.png', cnt=2, confidence=0.9, desc="instance",region=(0,0,800,800))
+        self.sleep_while(0.5, 20, pic_file='instance.png',  confidence=0.9, desc="instance")
+        pos = self.while_get_pic('instance.png', cnt=2, confidence=0.9, desc="instance", region=(0, 0, 800, 800))
         self.click_pos(pos, "open goad instance", 1)
         time.sleep(0.2)
-        pos = self.while_get_pic('run.png', cnt=2, confidence=0.9, desc="run", region=None)
+        pos = self.while_get_pic('lixianqidong.png', cnt=2, confidence=0.9, desc="run", region=None)
         self.click_pos(pos, "run", 1)
 
-        while True:
-            pos1 = self.while_get_pic('lixianbtn.png', cnt=2, confidence=0.9, desc="lixianbtn", region=None)
+        max_cnt=60
+        cnt=0
+        while cnt <max_cnt:
+            pos1 = self.while_get_pic('lixianbtn.png', cnt=1, confidence=0.9, desc="lixianbtn", region=None)
             if pos1 is not None:
+                pyautogui.write(user)
                 pyautogui.press("enter")
                 time.sleep(1)
-            pos2 = self.while_get_pic('duoren.png', cnt=2, confidence=0.9, desc="duoren", region=None)
+            pos2 = self.while_get_pic('duoren.png', cnt=1, confidence=0.9, desc="duoren", region=None)
             if pos2 is not None:
                 self.click_pos(pos2, desc="mem", click_cnt=2)
-                time.sleep(1)
-            pos3 = self.while_get_pic('mem.png', cnt=2, confidence=0.9, desc="duoren", region=None)
-            if pos3 is not None:
-                self.click_pos(pos3,desc="mem",click_cnt=2)
-                time.sleep(1)
-
-            pos = self.while_get_pic('xin.png', cnt=2, confidence=0.9, desc="duoren", region=None)
-            if pos is not None:
-                print("get xin")
                 break
-            time.sleep(0.2)
+            time.sleep(1)
+            cnt+=1
+
+        self.sleep_while(0.5, 30, pic_file='mem.png', confidence=0.9, desc="duoren")
+        pos3 = self.while_get_pic('mem.png', cnt=1, confidence=0.9, desc="duoren", region=None)
+        if pos3 is not None:
+            self.click_pos(pos3, desc="mem", click_cnt=2)
+
+        self.sleep_while(0.5, 30, pic_file='wanjiaxitong.png', confidence=0.9, desc="xin")
+        pos = self.while_get_pic('wanjiaxitong.png', cnt=1, confidence=0.9, desc="xin", region=None)
+        if pos is not None:
+            print("get xin")
+
         pyautogui.hotkey('ctrl', 'command', 'f')
-        time.sleep(1)
+        time.sleep(0.2)
         pyautogui.hotkey('ctrl', 'command', '/')
-        time.sleep(1)
+        time.sleep(0.2)
+        pass
+
+    def register_users(self):
+        index = 0
+        user_pre="panda"
+        for index in range (1,100):
+            user= user_pre+str(index).zfill(4)
+            self.close()
+            self.open(user)
+            # time.sleep(20)
+        # self.login_hub()
+            time.sleep(2)
+            self.register()
+            self.login_with_code()
+            # time.sleep(1)
+            self.choice_shengcun()
+            # time.sleep(1)
+            self.login_into_shengcun()
+            # self.goto_area_2()
+            time.sleep(20)
+            self.goto_area_2()
+            time.sleep(20)
         pass
 
 
 if __name__ == '__main__':
     cli = ForeverMemoryClient()
-    cli.run_v3()
-    #cli.run_sealonly()
+    cli.register_users()
+    #cli.run_v3()
+    # cli.run_sealonly()
